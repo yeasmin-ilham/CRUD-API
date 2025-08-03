30624ffa9dfc657be7e47f48c09bb1ca22d778e0
Yeasmin Ilham <farjanayeasminilham@gmail.com>
2025-08-03 11:37:10 +0600

    done

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express"
 const app = express();
+import userRouter from "../CRUD API/route/userinfo.js"
+
+app.use('/users' , userRouter)
 
 const PORT = 5000;
+
+app.get('/', (req, res) =>{
+res.send('Home')
+})
 
 app.listen(PORT, (req, res) =>{
     console.log(`Server running on port: ${PORT}`)
